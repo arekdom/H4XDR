1b3c82ad1699ac7b7d538c6eef166fc8e56e4bee
Arek <arek@Areks-iMac.local>
2021-01-17 13:35:32 +0000

    SwiftUI app

---
+++ H4XDR News/H4XDR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XDR_NewsApp.swift
+//  H4XDR News
+//
+//  Created by Arek on 15/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct H4XDR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ H4XDR News/NetworkManager.swift
@@ -0,0 +1,37 @@
+//
+//  NetworkManagement.swift
+//  H4XDR News
+//
+//  Created by Arek on 15/01/2021.
+//
+
+import Foundation
+
+class NetworkManager: ObservableObject {
+    
+    @Published var posts = [Post]()
+    
+    
+    func fetchData() {
+        if let url = URL(string: "https://hn.algolia.com/api/v1/search?tags=front_page") {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error == nil {
+                    let decoder = JSONDecoder()
+                    if let safeData = data {
+                        do {
+                    let results = try decoder.decode(Results.self, from: safeData)
+                            DispatchQueue.main.async {
+                                self.posts = results.hits
+                            }
+                         
+                        } catch {
+                             print(error)
+                }
+            }
+        }
+    }
+            task.resume()
+}
+}
+}
